ba97deda1eda994be37819bd2aae4326a092a3a4
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-07-14 20:24:41 +0530

    Create MealItemForm.js

---
+++ Meals/MealItem/MealItemForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const MealItemForm=(props)=>{
+    return(
+        <form>
+            <input />
+            <button>Add</button>
+        </form>
+
+    )
+}
+
+export default MealItemForm
