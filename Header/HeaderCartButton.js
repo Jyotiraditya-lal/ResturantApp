29bcd1f5903a15d6e5e9bf94038e4ce40cd711a4
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-07-15 15:06:12 +0530

    Update HeaderCartButton.js

--- Header/HeaderCartButton.js
+++ Header/HeaderCartButton.js
@@ -3,9 +3,13 @@ import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css"
 
 
+
+
+
 const HeaderCartButton=(props)=>{
+
     return(
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onDisplay}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
