c69c5838df34513da176aac45f64752aec5de024
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-07-16 15:54:42 +0530

    Update MealItemForm.js

--- Meals/MealItem/MealItemForm.js
+++ Meals/MealItem/MealItemForm.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React,{useContext} from "react";
 import classes from "./MealItemForm.module.css"
 import Input from "../../UI/Input";
+import CartContext from "../../../store/Context";
 
 const MealItemForm=(props)=>{
+
+    const cartCtx=useContext(CartContext)
+
+    const AddToCart=(event)=>{
+
+        event.preventDefault()
+        const amount=document.getElementById('amount_'+props.items.id).value
+        cartCtx.addItem( {...props.items, amount: amount})
+        
+    }
+    
+
     return(
         <form className={classes.form} >
             <Input label="Amount" input={{
-                id: 'amount',
+                id: 'amount_'+props.items.id,
                 type: 'number',
                 min: '1',
                 max: '5',
                 step: '1',
-                defaultValue: '1'
+                defaultValue: '0'
             }} />
-            <button>Add</button>
+            <button onClick={AddToCart}>Add</button>
         </form>
 
     )
