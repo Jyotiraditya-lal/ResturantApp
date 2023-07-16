018c62d04c12ecc191c21be598f955c0844ba0f2
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-07-16 15:49:30 +0530

    Update Cart.js

--- Cart/Cart.js
+++ Cart/Cart.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React,{useContext} from "react";
 import classes from './Cart.module.css'
 import Modal from "../UI/Modal";
+import CartContext from "../../store/Context";
+import CartItem from "./CartItem";
 
 const Cart=(props)=>{
+
+    const cartCtx=useContext(CartContext)
+
+    const RemoveFromCartHandler=(id)=>{}
+
+    const AddToCardHandler=(item)=>{}
+
+    let totalAmount=0
+    for(let i=0;i<cartCtx.items.length;i++){
+        totalAmount=totalAmount+(cartCtx.items[i].price)*(cartCtx.items[i].amount)
+    }
+
     const cartItems=<ul className={classes['cart-items']}>
-        {[{id: "c1", name: "sushi", amount: 2, price: 12.99}].map((item)=><li>
-            {item.name}
-         </li>)}</ul>
+        {cartCtx.items.map((item)=>
+           <CartItem 
+               key={item.id} 
+               name={item.name} 
+               price={item.price} 
+               amount={item.amount}
+               onRemove={RemoveFromCartHandler}
+               onAdd={AddToCardHandler} 
+            /> 
+         )}</ul>
 
     return(
         <Modal onRemove={props.onCancel}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.98</span>
+                <span>${totalAmount.toFixed(2)}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onCancel}>Cancel</button>
