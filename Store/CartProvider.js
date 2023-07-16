8af9729b4b4fcd6552d4fce8acb151dc1e2382a0
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-07-16 15:48:25 +0530

    Update CartProvider.js

--- Store/CartProvider.js
+++ Store/CartProvider.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React,{useState} from "react";
 import CartContext from "./Context";
+
 
 
 const CartProvider=(props)=>{
 
-    const AddItemsHandler=(item)=>{}
+    const [itemState,setItemState]= useState([])
+
+    const AddItemsHandler=(item)=>{
+        
+        const itemID=itemState.map(itm=>itm.id)
+
+        if(itemState.length>0){
+
+            if(itemID.includes(item.id)){
+
+              for(let i=0;i<itemState.length;i++){
+                 if(itemState[i].id===item.id){
+                    itemState[i].amount=Number(itemState[i].amount)+Number(item.amount)
+                    break;
+                   }   
+                }
+            }
+            else{
+                setItemState([...itemState,item])
+            }
+        }
+        else{
+            setItemState([...itemState,item])
+        }
+        
+        
+        console.log(itemState)
+    }
 
 
     const RemoveItemsHandler=(id)=>{}
     
     
     const cartContext={
-        items: [],
-        TotalAmount: 0,
-        addItems: AddItemsHandler,
-        removeItems: RemoveItemsHandler
+        items: itemState,
+        addItem: AddItemsHandler,
+        removeItem: RemoveItemsHandler
     }
     return(
         <CartContext.Provider value={cartContext}>
